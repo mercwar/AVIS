bad5128e113cc097576957747e1befcc13c8f896
CVBGOD <mercwar01@gmail.com>
2026-02-16 01:43:14 -0500

    Update fire-gem.asm

--- fire-gem.asm
+++ fire-gem.asm
@@ -1,105 +1,109 @@
 ; FILE: fire-gem.asm
-; IDENTITY: VERSION 3.3 // MEMORY-MAPPED DISPATCHER // HAHA!
-; ROLE: Load JSON instructions into RAM for high-speed Linux Installation.
+; IDENTITY: VERSION 3.4 // MASTER DISPATCHER // HAHA!
+; ROLE: Sequential Terminal Protocol - JSON-to-Shell Pipeline.
 
 section .data
     vault_path  db "fire-gem/artifacts/json/asm/", 0
+    sh_bin      db "/bin/bash", 0
     mod_path    db "fire-gem/artifacts/sh/fire-mod.sh", 0
     comp_path   db "fire-gem/artifacts/sh/fire-compile.sh", 0
     run_path    db "fire-gem/artifacts/sh/fire-run.sh", 0
-    sh_bin      db "/bin/bash", 0
 
 section .bss
     dir_buf     resb 4096
     child_pid   resq 1
-    mem_addr    resq 1      ; Address of the JSON in RAM
 
 section .text
     global _start
-    global FIRE_GEM_LOAD_MEM ; EXPORTED FOR 0001
 
 _start:
-    ; 1. OPEN VAULT DIRECTORY
+    ; 1. OPEN VAULT DIRECTORY (rax=2)
     mov rax, 2          
     mov rdi, vault_path
-    xor rsi, rsi
+    xor rsi, rsi        ; O_RDONLY
     syscall
     test rax, rax
-    js .exit
-    mov r8, rax         ; Save Vault FD
+    js exit_error       ; Jump to error if directory missing
+    mov r8, rax         ; Save Vault File Descriptor (FD)
 
-.scan_loop:
+scan_loop:
+    ; 2. READ DIRECTORY ENTRIES (rax=217)
     mov rax, 217        ; sys_getdents64
     mov rdi, r8
     mov rsi, dir_buf
     mov rdx, 4096
     syscall
     test rax, rax
-    jle .close_exit
-
-    ; --- MEMORY MAPPING PHASE ---
-    ; Here we would parse the filename and call LOAD_MEM
-    ; call FIRE_GEM_LOAD_MEM, <filename>
+    jle close_exit      ; End of Vault reached
 
     ; --- TERMINAL PROTOCOL: SEQUENTIAL EXECUTION ---
+    ; STEP A: FIRE-MOD
     push mod_path
     call fork_and_exec_worker
     
+    ; STEP B: FIRE-COMPILE
     push comp_path
     call fork_and_exec_worker
     
+    ; STEP C: FIRE-RUN
     push run_path
     call fork_and_exec_worker
 
-    jmp .scan_loop
+    jmp scan_loop       ; RESUME TO NEXT CJS BLOCK
 
-FIRE_GEM_LOAD_MEM:
-    ; 2. MAP JSON TO MEMORY (rax=9: sys_mmap)
-    ; This puts the JSON instructions in the heap for the ASM to devour
-    mov rax, 9          ; sys_mmap
-    xor rdi, rdi        ; addr = NULL
-    mov rsi, 8192       ; len = 8KB
-    mov rdx, 3          ; prot = PROT_READ | PROT_WRITE
-    mov r10, 34         ; flags = MAP_PRIVATE | MAP_ANONYMOUS
-    mov r8, -1          ; fd = -1
-    xor r9, r9          ; offset = 0
-    syscall
-    mov [mem_addr], rax ; Seated in RAM
-    ret
-
-.close_exit:
-    mov rax, 3
+close_exit:
+    mov rax, 3          ; sys_close
     mov rdi, r8
     syscall
 
-.exit:
-    mov rax, 60
+exit_success:
+    mov rax, 60         ; sys_exit
     xor rdi, rdi
     syscall
 
+exit_error:
+    mov rax, 60         ; sys_exit
+    mov rdi, 1          ; error code 1
+    syscall
+
+; --- HELPER: FORK -> EXEC -> WAIT ---
 fork_and_exec_worker:
-    pop r9
-    pop rdi             
+    pop r12             ; Save return address in r12
+    pop rdi             ; Get worker path from stack
+    
     mov rax, 57         ; sys_fork
     syscall
     test rax, rax
-    jz .child_worker
+    jz child_worker     ; If rax=0, we are in the child process
+    
+    ; PARENT LOGIC: WAIT4 (rax=61)
     mov [child_pid], rax
-    mov rax, 61         ; sys_wait4
+    mov rax, 61
     mov rdi, [child_pid]
-    xor rsi, rsi
-    xor rdx, rdx
-    xor r10, r10
-    syscall
-    push r9
+    xor rsi, rsi        ; status = NULL
+    xor rdx, rdx        ; options = 0
+    xor r10, r10        ; rusage = NULL
+    syscall             ; PAUSE UNTIL WORKER EXITS
+    
+    push r12            ; Restore return address
     ret
 
-.child_worker:
-    mov rax, 59
+child_worker:
+    ; EXECVE: /bin/bash <worker_path>
+    mov rax, 59         ; sys_execve
     mov rdi, sh_bin
-    push 0
-    push rdi
-    push sh_bin
-    mov rsi, rsp
-    xor rdx, rdx
+    
+    ; Build argv [bash, worker_path, NULL]
+    xor rax, rax
+    push rax            ; NULL terminator
+    push rdi            ; worker_path
+    push sh_bin         ; "bash"
+    mov rsi, rsp        ; rsi = argv pointer
+    xor rdx, rdx        ; envp = NULL
+    mov rax, 59         ; sys_execve
+    syscall
+    
+    ; If execve fails
+    mov rax, 60
+    mov rdi, 2
     syscall
