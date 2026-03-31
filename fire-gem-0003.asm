9eca9ad965f3bb4c37d1e6ceb1f091e71e9caf3d
CVBGOD <mercwar01@gmail.com>
2026-03-31 18:57:29 -0400

    Refactor fire-gem-0003.asm for version 2.0

--- fire-gem-0003.asm
+++ fire-gem-0003.asm
@@ -1,77 +1,44 @@
-; /*******************************************************************************
-;  * TYPE: LAW | CLASS: INGESTOR | NAME: fire-gem-0003.asm
-;  * IDENTITY: VERSION 2.0 // COORDINATE_MATERIALIZER_V2 // HAHA!
-;  * ROLE: Create Version 2 Vault and Materialize AVIS.avis.
-;  *******************************************************************************/
-
+; IDENTITY: VERSION 2.0 // COORDINATE_MATERIALIZER_V2 // HAHA!
+; ROLE: Create V2 Vault and seat AVIS.avis coordinate.
 section .data
-    ; DIRECTORY PATHS
     v2_dir      db "fire-gem/artifacts/json/cbord/reg/v2/", 0
     target_file db "fire-gem/artifacts/json/cbord/reg/v2/AVIS.avis", 0
-    
-    ; COORDINATE CONTENT BLOCK
-    content     db "/* ", 10
-                db "AVIS.FVS VERSION 2 - DO NOT REMOVE", 10
-                db "AVIS.FVS FIRE BEGIN AVIS COORDINATE", 10
-                db " * AVIS COORDINATE SYSTEM: https://github.io", 10
-                db " * SOURCE_FILE_NAME: AVIS.avis", 10
-                db " * VAULT_PATH: fire-gem/artifacts/json/cbord/reg/v2/", 10
-                db "AVIS.FVS FIRE END", 10
-                db " */", 0
+    content     db "/* AVIS.FVS VERSION 2 - COORDINATE SYSTEM SEATED */", 10, 0
     content_len equ $ - content
-
     ack_msg     db "[ACK] GEM-0003: SEATING VERSION 2 VAULT...", 10
     ack_len     equ 43
-
 section .bss
     fd_out      resq 1
-
 section .text
     global _start
-
 _start:
-    ; 1. SIGNAL INGESTION START
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
+    mov rax, 1
+    mov rdi, 1
     mov rsi, ack_msg
     mov rdx, ack_len
     syscall
-
-    ; 2. CREATE VERSION 2 DIRECTORY (sys_mkdir)
-    mov rax, 83         ; sys_mkdir
+    ; MKDIR V2
+    mov rax, 83
     mov rdi, v2_dir
-    mov rsi, 0755o      ; rwxr-xr-x permissions
+    mov rsi, 0755o
     syscall
-    ; We ignore error if directory already exists (EEXIST)
-
-    ; 3. CREATE / OPEN AVIS.avis IN V2 VAULT
-    mov rax, 2          ; sys_open
+    ; CREATE AVIS.avis
+    mov rax, 2
     mov rdi, target_file
-    mov rsi, 65         ; O_CREAT | O_WRONLY
-    mov rdx, 0644o      ; rw-r--r-- permissions
+    mov rsi, 65
+    mov rdx, 0644o
     syscall
-    test rax, rax
-    js exit_error
     mov [fd_out], rax
-
-    ; 4. DROP COORDINATE DATA
-    mov rax, 1          ; sys_write
+    ; WRITE CONTENT
+    mov rax, 1
     mov rdi, [fd_out]
     mov rsi, content
     mov rdx, content_len
     syscall
-
-    ; 5. CLOSE AND SEAL
-    mov rax, 3          ; sys_close
+    ; CLOSE & EXIT
+    mov rax, 3
     mov rdi, [fd_out]
     syscall
-
-    ; 6. TERMINATE (CLEAN EXIT)
-    mov rax, 60         ; sys_exit
-    xor rdi, rdi
-    syscall
-
-exit_error:
     mov rax, 60
-    mov rdi, 1
+    xor rdi, rdi
     syscall
