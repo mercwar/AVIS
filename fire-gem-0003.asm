a2c5155174c6f58232a4214cfb3dc2f17532867c
CVBGOD <mercwar01@gmail.com>
2026-03-31 19:09:31 -0400

    Refactor fire-gem-0003.asm for version 2.0

--- fire-gem-0003.asm
+++ fire-gem-0003.asm
@@ -1,5 +1,4 @@
 ; IDENTITY: VERSION 2.0 // COORDINATE_MATERIALIZER_V2 // HAHA!
-; ROLE: Create V2 Vault and seat AVIS.avis coordinate.
 section .data
     v2_dir      db "fire-gem/artifacts/json/cbord/reg/v2/", 0
     target_file db "fire-gem/artifacts/json/cbord/reg/v2/AVIS.avis", 0
@@ -7,38 +6,29 @@ section .data
     content_len equ $ - content
     ack_msg     db "[ACK] GEM-0003: SEATING VERSION 2 VAULT...", 10
     ack_len     equ 43
-section .bss
-    fd_out      resq 1
+
 section .text
     global _start
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, ack_msg
-    mov rdx, ack_len
-    syscall
-    ; MKDIR V2
+    ; mkdir -p v2
     mov rax, 83
     mov rdi, v2_dir
     mov rsi, 0755o
     syscall
-    ; CREATE AVIS.avis
+    ; create AVIS.avis
     mov rax, 2
     mov rdi, target_file
     mov rsi, 65
     mov rdx, 0644o
     syscall
-    mov [fd_out], rax
-    ; WRITE CONTENT
+    mov r8, rax
+    ; write & exit
     mov rax, 1
-    mov rdi, [fd_out]
+    mov rdi, r8
     mov rsi, content
     mov rdx, content_len
-    syscall
-    ; CLOSE & EXIT
-    mov rax, 3
-    mov rdi, [fd_out]
     syscall
     mov rax, 60
     xor rdi, rdi
     syscall
+
