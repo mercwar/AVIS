8b0e0a7fb6df222df4409ff6af3334b90531a6ab
CVBGOD <mercwar01@gmail.com>
2026-03-31 18:55:51 -0400

    Refactor fire-mod.sh to update directory structure

--- fire-gem/artifacts/sh/fire-mod.sh
+++ fire-gem/artifacts/sh/fire-mod.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# /*******************************************************************************
-#  * TYPE: SERVICE | CLASS: MOD-ENGINE | NAME: fire-mod.sh
-#  * IDENTITY: VERSION 4.1 // PERMISSION_SEATED // HAHA!
-#  *******************************************************************************/
+# IDENTITY: VERSION 4.1 // PERMISSION_SEATED // HAHA!
+# ROLE: Atomic Seating of Vaults and Permissions.
 echo "[AVIS] MOD_SERVICE: Seating Permissions..."
-mkdir -p ./avis/ ./logs/ ./fire-gem/artifacts/json/cbord/reg/ ./fire-gem/artifacts/json/reg/
+mkdir -p ./avis/ ./logs/ ./fire-gem/artifacts/json/cbord/reg/v2/ ./fire-gem/artifacts/json/reg/
 chmod +x ./sh/*.sh 2>/dev/null
 echo "FIRE-MOD: System permissions seated. [EXIT]"
