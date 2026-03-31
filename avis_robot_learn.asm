0d719d2877d583e72faefeb4c14b620398b5b3aa
CVBGOD <mercwar01@gmail.com>
2026-03-31 19:10:50 -0400

    Refactor avis_robot_learn.asm for clarity and updates
    Removed old comments and functions related to device registration and alerting. Updated exit procedure to use a message output.

--- avis_robot_learn.asm
+++ avis_robot_learn.asm
@@ -1,56 +1,16 @@
-; FILE: avis_robot_learn.asm
-; IDENTITY: VERSION 1 // GEMINI_CGI_SCROLL // HAHA!
-; ROLE: Robotic Learning Module for AVIS Ingestion
-; ENGINE: AVIS-2026 / AIFVS-MODE
-
+; IDENTITY: VERSION 1 // ROBOTIC LEARNING // HAHA!
 section .data
-    ; [DEVICE] Registration Data for fire-gem.ini
-    dev_reg      db "[DEVICE]", 0x0A
-                 db "ID=ROBOT_LEARN_01", 0x0A
-                 db "DRIVER=AVIS_FVS_MOD", 0x0A, 0
-    
-    ; AVIS_ALERT Payload (Gold Level 2 HTML style)
-    alert_msg    db "<div class='avis-alert'>HARDWARE SEATED: ROBOT_LEARN_01 ONLINE</div>", 0
+    msg db "<div class='avis-alert'>HARDWARE SEATED: ROBOT_LEARN_01 ONLINE</div>", 10
+    len equ $ - msg
 
 section .text
     global _start
-
 _start:
-    ; STEP 1: HELLO (Declaration of Readiness)
-    call avis_handshake_init
-
-    ; STEP 2: REGISTER DEVICE (Seating in fire-gem.ini)
-    ; Simulated: In a real FGEO, this would pipe to the CJS Processor (fire-cyhy.asm)
-    mov rdi, dev_reg
-    call mock_fire_gem_seat
-
-    ; STEP 3: GEM_STRIKE_GCC (Self-Build Vector)
-    ; Study fire-gem.asm: EXPORT_GCC_COMPILE vector
-    call gem_strike_gcc_call
-
-    ; STEP 4: TRIGGER AVIS_ALERT (Visual Surface Gateway)
-    ; Study htdocs/AVIS_FVS_ALERT/AVIS_FVS_MOD.js
-    mov rdi, alert_msg
-    call trigger_fvs_surface
-
-    ; STEP 5: EXIT
-    mov rax, 60         ; sys_exit
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, len
+    syscall
+    mov rax, 60
     xor rdi, rdi
     syscall
-
-avis_handshake_init:
-    ; Protocol Handshake Law v1 Compliance
-    ; Output: TYPE: HELLO | VERSION: 1.00
-    ret
-
-mock_fire_gem_seat:
-    ; Function to seat driver in Supreme Registry
-    ret
-
-gem_strike_gcc_call:
-    ; Call to Master Dispatcher EXPORT_GCC_COMPILE
-    ret
-
-trigger_fvs_surface:
-    ; CGI Alerting via AVIS_FVS_ALERT_INI.js
-    ret
