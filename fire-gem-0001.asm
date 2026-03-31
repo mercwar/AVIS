ef4c13ce7219d9166f8659e7fea7eea6ed31c698
CVBGOD <mercwar01@gmail.com>
2026-03-31 16:10:45 -0400

    Update version and improve comments in assembly code

--- fire-gem-0001.asm
+++ fire-gem-0001.asm
@@ -1,6 +1,6 @@
 ; /*******************************************************************************
 ;  * TYPE: LAW | CLASS: MIRROR-INGESTOR | NAME: fire-gem-0001.asm
-;  * IDENTITY: VERSION 4.70 // JOE TRON // CVBGOD // HAHA!
+;  * IDENTITY: VERSION 4.71 // FIXED SCOPE // HAHA!
 ;  * ROLE: Materialize JSON Namespace into CBORD Vault with Name Parity.
 ;  *******************************************************************************/
 
@@ -11,8 +11,8 @@ section .data
 
 section .bss
     dir_buf     resb 4096
-    name_buf    resb 256    ; Current filename being mirrored
-    out_path    resb 512    ; Materialized output path
+    name_buf    resb 256    
+    out_path    resb 512    
     fd_in       resq 1
     fd_out      resq 1
 
@@ -20,7 +20,7 @@ section .text
     global _start
 
 _start:
-    ; 1. OPEN INPUT VAULT (rax=2)
+    ; 1. OPEN INPUT VAULT
     mov rax, 2
     mov rdi, in_dir
     xor rsi, rsi
@@ -28,35 +28,35 @@ _start:
     mov r8, rax         ; Input Dir FD
 
 .dir_loop:
-    ; 2. CRAWL NAMESPACE (rax=217)
+    ; 2. CRAWL NAMESPACE (getdents64)
     mov rax, 217
     mov rdi, r8
     mov rsi, dir_buf
     mov rdx, 4096
     syscall
+    
     test rax, rax
-    jle .exit_pulse
-
-    ; [NAMESPACE MATERIALIZATION]
-    ; For each [name] found in dir_buf:
-    ; A. Construct out_path: "fire-gem/artifacts/cbord/reg/" + [name]
-    ; B. Open out_path for writing
-    ; C. Drop ELF Magic + Transcoded Logic
+    jle .exit_pulse     ; Jumps to the local label below
 
     call materialize_mirror
     jmp .dir_loop
 
+.exit_pulse:            ; Correctly scoped under _start
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
 materialize_mirror:
-    ; ATOMIC DROP: Maintains exact filename delimiters
-    mov rax, 2          ; sys_open (Create Mirror)
-    mov rdi, out_path   ; Constructed from name_buf
+    ; ATOMIC DROP
+    mov rax, 2          
+    mov rdi, out_path   
     mov rsi, 65         ; O_CREAT | O_WRONLY
     mov rdx, 0644o
     syscall
     mov [fd_out], rax
 
     ; 3. SEAT ELF HANDSHAKE
-    mov rax, 1          ; sys_write
+    mov rax, 1          
     mov rdi, [fd_out]
     mov rsi, elf_magic
     mov rdx, 4
@@ -67,8 +67,3 @@ materialize_mirror:
     mov rdi, [fd_out]
     syscall
     ret
-
-.exit_pulse:
-    mov rax, 60
-    xor rdi, rdi
-    syscall
