abca1122a7bcec36885e473669d6f28439ad9e9a
CVBGOD <mercwar01@gmail.com>
2026-03-31 19:14:59 -0400

    Update fire-gem-0003.asm

--- fire-gem-0003.asm
+++ fire-gem-0003.asm
@@ -1,34 +1,22 @@
-; IDENTITY: VERSION 2.0 // COORDINATE_MATERIALIZER_V2 // HAHA!
+; IDENTITY: VERSION 2.0 // V2_MATERIALIZER // HAHA!
 section .data
-    v2_dir      db "fire-gem/artifacts/json/cbord/reg/v2/", 0
-    target_file db "fire-gem/artifacts/json/cbord/reg/v2/AVIS.avis", 0
-    content     db "/* AVIS.FVS VERSION 2 - COORDINATE SYSTEM SEATED */", 10, 0
-    content_len equ $ - content
-    ack_msg     db "[ACK] GEM-0003: SEATING VERSION 2 VAULT...", 10
-    ack_len     equ 43
-
+    v2_file db "fire-gem/artifacts/json/cbord/reg/v2/AVIS.avis", 0
+    content db "AVIS_V2_COORDINATE_SEATED", 10, 0
+    len     equ $ - content
 section .text
     global _start
 _start:
-    ; mkdir -p v2
-    mov rax, 83
-    mov rdi, v2_dir
-    mov rsi, 0755o
-    syscall
-    ; create AVIS.avis
-    mov rax, 2
-    mov rdi, target_file
-    mov rsi, 65
+    mov rax, 2          ; sys_open
+    mov rdi, v2_file
+    mov rsi, 65         ; O_CREAT | O_WRONLY
     mov rdx, 0644o
     syscall
-    mov r8, rax
-    ; write & exit
-    mov rax, 1
-    mov rdi, r8
+    mov rdi, rax
+    mov rax, 1          ; sys_write
     mov rsi, content
-    mov rdx, content_len
+    mov rdx, len
     syscall
-    mov rax, 60
+    mov rax, 60         ; sys_exit
     xor rdi, rdi
     syscall
 
