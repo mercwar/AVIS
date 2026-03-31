30bbd2b895a273a68be61d0aaa63862468f600ed
CVBGOD <mercwar01@gmail.com>
2026-03-31 18:29:24 -0400

    Update fire-start.sh

--- fire-start.sh
+++ fire-start.sh
@@ -1,69 +1,55 @@
 #!/bin/bash
-# /*******************************************************************************
-#  * TYPE: ENGINE | CLASS: MASTER-PULSE | NAME: fire-start.sh
-#  * IDENTITY: VERSION 3 // FIRE-START // MASTER-PULSE // CVBGOD
-#  *******************************************************************************/
+# IDENTITY: VERSION 3.9 // CJS-STACK-COMPLIANT // HAHA!
+# ROLE: Execute AVIS_CJS_OBJECT Program Stack from fire-cjs.json
 
 CJS_FILE="fire-cjs.json"
 
-# --- WIN43 / CJS FUNCTIONS ---
-register_class() {
-    local src=$1
-    local name=$2
-    echo "WIN43: Registering Class $name from $src..."
-    nasm -f elf64 "$src" -o temp.o && ld temp.o -o "${src%.asm}_bin"
-    rm temp.o
-    echo "BASH: [ACK] WM_CLASS_REGISTERED: $name"
-}
+# --- CORE LOGIC ---
+echo "[AVIS] PULSE: Ingesting CJS Program Stack..."
 
-create_proc_threads() {
-    local id=$1
-    local idx=$2
-    echo "WIN43: WM_CREATE_PROC for $id"
-    (
-        T_COUNT=$(jq ".AVIS_CJS_OBJECT.PROGRAM_STACK[$idx].THREADS | length" $CJS_FILE)
-        for (( j=0; j<$T_COUNT; j++ )); do
-            T_FILE=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$idx].THREADS[$j].FUNCTION" $CJS_FILE)
-            if [ -f "$T_FILE" ]; then
-                chmod +x "$T_FILE"
-                ./"$T_FILE" &
-                echo "THREAD_ACK: $T_FILE running [PID: $!]"
-            fi
-        done
-        wait
-    ) &
-}
-
-# --- MAIN CJS LOOP ---
 IDX=0
 while true; do
-    # Check if JQ can find the file to prevent the previous error crash
-    if [ ! -f "$CJS_FILE" ]; then
-        echo "BASH: [NACK] $CJS_FILE NOT FOUND. TERMINATING PULSE."
-        exit 1
+    # Capture current command code
+    CODE=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].CODE // \"null\"" "$CJS_FILE")
+    
+    # Exit if we hit the end of the stack
+    if [ "$CODE" == "null" ] || [ "$CODE" == "EXIT" ]; then 
+        echo "[AVIS] wm_macro_ack: Stack reached EXIT sentinel."
+        break 
     fi
-
-    CODE=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].CODE" $CJS_FILE)
-    if [ "$CODE" == "null" ] || [ "$CODE" == "EXIT" ]; then break; fi
 
     case $CODE in
         "WM_REGISTER_CLASS")
-            SRC=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].SOURCE" $CJS_FILE)
-            CLS=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].CLASS_NAME" $CJS_FILE)
-            register_class "$SRC" "$CLS"
+            SRC=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].SOURCE" "$CJS_FILE")
+            CLS=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].CLASS_NAME" "$CJS_FILE")
+            echo "WIN43: Registering Class $CLS from $SRC..."
+            
+            # ASM FORGE: Smith to the /avis/ vault
+            nasm -f elf64 "$SRC" -o temp.o && ld temp.o -o "avis/${SRC%.asm}.exe"
+            rm -f temp.o
+            echo "BASH: [ACK] $CLS Seated in /avis/"
             ;;
+
         "WM_CREATE_PROC")
-            ID=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].ID" $CJS_FILE)
-            create_proc_threads "$ID" "$IDX"
+            ID=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].ID" "$CJS_FILE")
+            echo "WIN43: Initializing $ID"
+            
+            # Spawning the "Threads" (Shell Scripts defined in the JSON)
+            T_COUNT=$(jq ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].THREADS | length" "$CJS_FILE" 2>/dev/null || echo 0)
+            for (( j=0; j<$T_COUNT; j++ )); do
+                FUNC=$(jq -r ".AVIS_CJS_OBJECT.PROGRAM_STACK[$IDX].THREADS[$j].FUNCTION" "$CJS_FILE")
+                
+                if [ -f "./$FUNC" ]; then
+                    chmod +x "./$FUNC"
+                    # Execution with real-time log flush
+                    nohup stdbuf -oL -eL "./$FUNC" > "logs/thread-$FUNC.log" 2>&1 &
+                    echo "THREAD_ACK: $FUNC running [PID: $!]"
+                fi
+            done
             ;;
     esac
     IDX=$((IDX + 1))
 done
 
-# Seal and Dispatch
-if [ -f "./fire-end.sh" ]; then
-    chmod +x fire-end.sh
-    ./fire-end.sh
-else
-    echo "BASH: [NACK] fire-end.sh MISSING."
-fi
+# Seal the Pulse
+[ -f "./fire-end.sh" ] && chmod +x fire-end.sh && ./fire-end.sh
