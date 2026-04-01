5bf8c161f99dac568738bb9affda274de2588758
CVBGOD <mercwar01@gmail.com>
2026-03-31 20:19:15 -0400

    Update fire-gem-0003.asm

--- fire-gem-0003.asm
+++ fire-gem-0003.asm
@@ -1,9 +1,13 @@
-; IDENTITY: VERSION 5.2 // VERIFIED_INGESTOR // HAHA!
+; /*******************************************************************************
+;  * TYPE: LAW | CLASS: INGESTOR | NAME: fire-gem-0003.asm
+;  * IDENTITY: VERSION 5.2 // VERIFIED_INGESTOR // HAHA!
+;  *******************************************************************************/
+
 section .data
     source_file db "avis.AVIS", 0
-    ack_msg     db "[ACK] GEM-0003: avis.AVIS LOADED. STREAMING BNF...", 10, 0
+    ack_msg     db "[ACK] GEM-0003: avis.AVIS LOADED. STREAMING BNF...", 10
     ack_len     equ 48
-    err_msg     db "[NACK] GEM-0003: FATAL - avis.AVIS NOT FOUND", 10, 0
+    err_msg     db "[NACK] GEM-0003: FATAL - avis.AVIS NOT FOUND", 10
     err_len     equ 43
 
 section .bss
@@ -12,33 +16,34 @@ section .bss
 
 section .text
     global _start
+
 _start:
-    ; 1. ATTEMPT OPEN
-    mov rax, 2          ; sys_open
+    ; 1. OPEN TEACHER FILE
+    mov rax, 2
     mov rdi, source_file
-    xor rsi, rsi        ; O_RDONLY
+    xor rsi, rsi
     syscall
     test rax, rax
-    js .file_error      ; If RAX < 0, handle error
+    js .file_error
     mov [fd_in], rax
 
-    ; 2. SIGNAL SUCCESSFUL LOAD
+    ; 2. SIGNAL SUCCESS
     mov rax, 1
     mov rdi, 1
     mov rsi, ack_msg
     mov rdx, ack_len
     syscall
 
-    ; 3. READ & STREAM
-    mov rax, 0          ; sys_read
+    ; 3. READ & STREAM TO STDOUT (Captured by Zero-Buffer Log)
+    mov rax, 0
     mov rdi, [fd_in]
     mov rsi, buffer
     mov rdx, 4096
     syscall
     
-    mov rdx, rax        ; Bytes read
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
+    mov rdx, rax
+    mov rax, 1
+    mov rdi, 1
     mov rsi, buffer
     syscall
 
