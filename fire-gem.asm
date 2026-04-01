e21e2f4a1ca107f3b7d7d94f9db5eb969afaf0b3
CVBGOD <mercwar01@gmail.com>
2026-03-31 21:17:34 -0400

    Update fire-gem.asm

--- fire-gem.asm
+++ fire-gem.asm
@@ -12,32 +12,10 @@ section .data
 msg_header: db "[AVIS_V2] JOE TRON FORGE ONLINE",10,0
 
 ; --------------------------------------------------------------------
-; This command does EVERYTHING:
-;
-; 1. FORGE_OUT='AVIS/VERSION 2/ASM/FORGE/OUT'
-; 2. GEM_OUT='AVIS/VERSION 2/ASM/FIRE-GEM/OUT'
-; 3. LOG='AVIS/VERSION 2/ASM/fire-gem.log'
-; 4. mkdir -p GEM_OUT
-; 5. echo header > LOG
-; 6. For each .bin in FORGE_OUT:
-;       - Copy to GEM_OUT
-;       - Execute it
-;       - Append output to LOG
+; ONE SINGLE-LINE, NASM-SAFE COMMAND STRING
 ; --------------------------------------------------------------------
 
-cmd_str: db \
-"FORGE_OUT='AVIS/VERSION 2/ASM/FORGE/OUT'; ", \
-"GEM_OUT='AVIS/VERSION 2/ASM/FIRE-GEM/OUT'; ", \
-"LOG='AVIS/VERSION 2/ASM/fire-gem.log'; ", \
-"mkdir -p \"$GEM_OUT\"; ", \
-"echo '[AVIS_V2] LOG START' > \"$LOG\"; ", \
-"for f in \"$FORGE_OUT\"/*.bin; do ", \
-"  [ -e \"$f\" ] || continue; ", \
-"  base=$(basename \"$f\"); ", \
-"  cp \"$f\" \"$GEM_OUT/$base\"; ", \
-"  echo \"[AVIS_V2] EXEC: $base\" >> \"$LOG\"; ", \
-"  \"$GEM_OUT/$base\" >> \"$LOG\" 2>&1; ", \
-"done",0
+cmd_str: db "FORGE_OUT='AVIS/VERSION 2/ASM/FORGE/OUT'; GEM_OUT='AVIS/VERSION 2/ASM/FIRE-GEM/OUT'; LOG='AVIS/VERSION 2/ASM/fire-gem.log'; mkdir -p \"$GEM_OUT\"; echo '[AVIS_V2] LOG START' > \"$LOG\"; for f in \"$FORGE_OUT\"/*.bin; do [ -e \"$f\" ] || continue; base=$(basename \"$f\"); cp \"$f\" \"$GEM_OUT/$base\"; echo \"[AVIS_V2] EXEC: $base\" >> \"$LOG\"; \"$GEM_OUT/$base\" >> \"$LOG\" 2>&1; done",0
 
 section .text
 
