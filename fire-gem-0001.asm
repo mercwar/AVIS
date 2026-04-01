4a2c6e58af154d7d8d55bd53905d8a485baedec8
CVBGOD <mercwar01@gmail.com>
2026-03-31 20:25:34 -0400

    Update fire-gem-0001.asm

--- fire-gem-0001.asm
+++ fire-gem-0001.asm
@@ -1,12 +1,14 @@
 ; /*******************************************************************************
 ;  * TYPE: LAW | CLASS: MIRROR-INGESTOR | NAME: fire-gem-0001.asm
-;  * IDENTITY: VERSION 4.75 // RESTORED // HAHA!
+;  * IDENTITY: VERSION 4.8 // ERROR_PULSE_ENABLED // HAHA!
 ;  *******************************************************************************/
 
 section .data
     in_dir      db "fire-gem/artifacts/json/reg/", 0
     ack_msg     db "[ACK] GEM-0001: THREAD ESCALATION INITIALIZED...", 10
     ack_len     equ 48
+    err_msg     db "[NACK] GEM-0001: FATAL - VAULT PATH NOT FOUND", 10
+    err_len     equ 44
 
 section .bss
     dir_buf     resb 4096
@@ -15,32 +17,42 @@ section .text
     global _start
 
 _start:
-    ; 1. SIGNAL PULSE
-    mov rax, 1
-    mov rdi, 1
+    ; 1. OPEN VAULT FIRST (Check existence before pulsing)
+    mov rax, 2          ; sys_open
+    mov rdi, in_dir
+    xor rsi, rsi        ; O_RDONLY
+    syscall
+    test rax, rax
+    js .fatal_error     ; If negative, the directory doesn't exist
+    mov r8, rax         ; Save FD
+
+    ; 2. SIGNAL SUCCESSFUL HANDSHAKE
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
     mov rsi, ack_msg
     mov rdx, ack_len
     syscall
 
-    ; 2. OPEN VAULT
-    mov rax, 2
-    mov rdi, in_dir
-    xor rsi, rsi
-    syscall
-    test rax, rax
-    js exit_error
-    mov r8, rax
-
-    ; 3. CRAWL & EXIT (Seals pulse to prevent loop)
-    mov rax, 3
+    ; 3. CRAWL & CLOSE
+    ; (Placeholder for getdents logic)
+    
+    mov rax, 3          ; sys_close
     mov rdi, r8
     syscall
     
-    mov rax, 60
+    mov rax, 60         ; sys_exit
     xor rdi, rdi
     syscall
 
-exit_error:
-    mov rax, 60
+.fatal_error:
+    ; 4. SIGNAL ERROR (This fixes the empty log)
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
+    mov rsi, err_msg
+    mov rdx, err_len
+    syscall
+
+    mov rax, 60         ; sys_exit
     mov rdi, 1
     syscall
+
